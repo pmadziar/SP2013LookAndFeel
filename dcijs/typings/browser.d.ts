e09c031768e8773b21fe667027fbcd529fce36eb
pmadziar <pmadziar@madziar.com>
2016-02-15 12:32:26 -0400

    rm

--- dcijs/typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser\ambient\SharePoint\SharePoint.d.ts" />
-/// <reference path="browser\ambient\jquery\jquery.d.ts" />
-/// <reference path="browser\ambient\jsdom\jsdom.d.ts" />
-/// <reference path="browser\ambient\node\node.d.ts" />
-/// <reference path="browser\definitions\gulp\gulp.d.ts" />
--- dcijs/typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="main\ambient\SharePoint\SharePoint.d.ts" />
-/// <reference path="main\ambient\jquery\jquery.d.ts" />
-/// <reference path="main\ambient\jsdom\jsdom.d.ts" />
-/// <reference path="main\ambient\node\node.d.ts" />
-/// <reference path="main\definitions\gulp\gulp.d.ts" />
